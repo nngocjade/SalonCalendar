de30934424734d26af2e8452c9e2920c11f2a208
nngocjade <nngocjade@gmail.com>
2020-11-01 11:47:49 +0800

    adding pseudo code for appointment pop up preview

---
+++ dist/js/app.js
@@ -0,0 +1,11 @@
+// DAILY TIME SLOT - ON CLICK - POP UP MODAL
+
+// Attach click listener to time slot
+
+// Attach modal add appt (if time slot is empty)
+   // if time slot taken 
+     // (else, have client first name and first initial of last name as placeholder) 
+     // attach click-listener
+      // on-click- preview appointment details
+        // (appointment detail pop up includes:
+             // )
